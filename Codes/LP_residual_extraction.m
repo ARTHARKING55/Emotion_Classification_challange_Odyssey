a8d031067f773276e95b59c1fe3670651c22c39e
Arth Juhul Shah <127669269+ARTHARKING55@users.noreply.github.com>
2024-02-18 03:54:04 +0530

    Create LP_residual_extraction.m

---
+++ Codes/LP_residual_extraction.m
@@ -0,0 +1,13 @@
+function [res, predicted_y_transpose] = LP_residual_extraction(y, fs)
+    P = 18;
+    ycorr = xcorr(y, P); % gives (2P+1) long autocorrelation, where (P+1)th position of ycorr is rx(0)
+    Af = flip(ycorr(1:(P+1))); % since (P+1)th position is rx(0), it is the highest value. We flip it so that the first value is rx(0), so that the toeplitz matrix comes out correct
+    A = toeplitz(Af(1:P));
+    r = Af(2:P+1);
+    L = -inv(A) * r; % multiplying inverse of A with r to get coeff
+    L = L';
+    LPCoeffs(1, 1:length([1, L])) = [1, L];
+    predicted_y = filter([0 -LPCoeffs(2:end)], 1, y);  % Estimated signal
+    predicted_y_transpose = predicted_y.';
+    res = y - predicted_y; % Prediction error
+end
